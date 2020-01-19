2d9c34f87eabeead40dbcdb73cef368909acf05b
Konstantinos Psimoulis <kosta@noima.com>
2020-01-19 09:19:57 -0500

    Adding uploader code

---
+++ src/index.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+var util = require('util');
+var exec = require('child_process').exec;
+const { process } = require("./jobs");
+
+var port = process.env.PORT || 3000;
+
+Files = {};
+
+app.use('/Video', express.static('Video'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/upload', function(req, res){
+  res.sendFile(__dirname + '/upload.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  //Events will go here
+  socket.on('Start', function (data) { //data contains the variables that we passed through in the html file
+    var Name = data['Name'];
+    Files[Name] = {  //Create a new Entry in The Files Variable
+        FileSize : data['Size'],
+        Data   : "",
+        Downloaded : 0
+    }
+    var Place = 0;
+    try{
+        var Stat = fs.statSync('Temp/' +  Name);
+        if(Stat.isFile())
+        {
+            Files[Name]['Downloaded'] = Stat.size;
+            Place = Stat.size / 524288;
+        }
+    }
+    catch(er){} //It's a New File
+    fs.open("Temp/" + Name, "a", 0755, function(err, fd){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            Files[Name]['Handler'] = fd; //We store the file handler so we can write to it later
+            socket.emit('MoreData', { 'Place' : Place, Percent : 0 });
+        }
+    });
+  });
+
+  socket.on('Upload', function (data){
+    var Name = data['Name'];
+    Files[Name]['Downloaded'] += data['Data'].length;
+    Files[Name]['Data'] += data['Data'];
+    if(Files[Name]['Downloaded'] == Files[Name]['FileSize']) //If File is Fully Uploaded
+    {
+        fs.write(Files[Name]['Handler'], Files[Name]['Data'], null, 'Binary', function(err, Writen){
+          var input = fs.createReadStream("Temp/" + Name);
+          var output = fs.createWriteStream("Video/" + Name);
+
+          input.pipe(output);
+          input.on("end", function() {
+              console.log("Uploaded file: Video/" + Name );
+              process(function(gesture){
+                //console.log("response is here man ", tired);
+                var answer = { "gesture": gesture };
+                socket.emit('GotGesture', answer);
+              });
+              fs.unlink("Temp/" + Name, function ()
+              {
+                exec("ffmpeg -i Video/" + Name  + " -ss 00:00:01.01 -y -f image2 -vcodec mjpeg -vframes 1 Video/" + Name  + ".jpg", function(err){
+                  socket.emit('Done', {'Image' : 'Video/' + Name + '.jpg'});
+                });
+            });
+          });
+      });
+
+    }
+    else if(Files[Name]['Data'].length > 10485760){ //If the Data Buffer reaches 10MB
+        fs.write(Files[Name]['Handler'], Files[Name]['Data'], null, 'Binary', function(err, Writen){
+            Files[Name]['Data'] = ""; //Reset The Buffer
+            var Place = Files[Name]['Downloaded'] / 524288;
+            var Percent = (Files[Name]['Downloaded'] / Files[Name]['FileSize']) * 100;
+            socket.emit('MoreData', { 'Place' : Place, 'Percent' :  Percent});
+        });
+    }
+    else
+    {
+        var Place = Files[Name]['Downloaded'] / 524288;
+        var Percent = (Files[Name]['Downloaded'] / Files[Name]['FileSize']) * 100;
+        socket.emit('MoreData', { 'Place' : Place, 'Percent' :  Percent});
+    }
+  });
+
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
---
+++ src/jobs.js
@@ -0,0 +1,111 @@
+var fs = require("fs");
+var request = require("request");
+const { readJSON, overwriteJSON } = require("./config/compare");
+//let PICTURE = "person.jpg"
+let PICTURE = "movie.mov"
+let file = fs.createReadStream(`${PICTURE}`);
+let TOKEN = '';
+
+var jobRequest = {
+  method: "POST",
+  url: "https://api.wrnch.ai/v1/jobs",
+  headers: {
+    "Content-Type":
+      "multipart/form-data; boundary=--------------------------780487085191623441098948",
+    Authorization: TOKEN
+  },
+  formData: {
+    work_type: "json",
+    heads: "true",
+    est_3d: "true",
+    media: file
+  }
+};
+
+var loginRequest = {
+    method: 'POST',
+    url: 'https://api.wrnch.ai/v1/login',
+    headers:
+    {
+        'Content-Type': 'application/json'
+    },
+    body: { api_key: '2e11e90c-6d82-44b8-9471-9b558126c70a' },
+    json: true
+};
+
+function getJob(jobID, jobToken, callback) {
+    var jobResponse = {
+        method: 'GET',
+        url: `https://api.wrnch.ai/v1/jobs/${jobID}`,
+        qs: { work_type: 'json' },
+        headers:
+        {
+            Authorization: jobToken
+        }
+    };
+
+    request(jobResponse, function (error, response, body) {
+        if (error) throw new Error(error);
+        let jsonBody = JSON.parse(body);
+        console.log('jsonBody', jsonBody);
+        callback(analyzer(jsonBody));
+    });
+}
+
+function analyzer(jsonBody) {
+
+    // Get Gesture and send it back
+    $gesture = 0
+    return response($gesture);
+}
+
+function response(id) {
+    let MOVE_LEFT = 0;
+    let MOVE_RIGHT = 1;
+
+    if (id === MOVE_LEFT) {
+        console.log("Status: MOVE_LEFT");
+        return MOVE_LEFT;
+    } else {
+        console.log("Status: MOVE_RIGHT");
+        return MOVE_RIGHT;
+    }
+}
+
+
+/****************** Main function  ********/
+function process(callback){
+
+    request(loginRequest, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        TOKEN = 'Bearer ' + body.access_token
+        console.log('TOKEN', TOKEN);
+
+        jobRequest.headers.Authorization = TOKEN;
+
+        request(jobRequest, function (error, response, body) {
+            if (error) throw new Error(error);
+            let jsonBody = JSON.parse(body);
+            console.log("jsonBody", jsonBody);
+            console.log("Job Id", jsonBody.job_id);
+            console.log('ZZZzzZzzZZZz for 5 seconds');
+            setTimeout(function () {
+                console.log('sending TOKEN', TOKEN);
+                getJob(jsonBody.job_id, TOKEN, function (answer) {
+                    console.log("Callbackhell answer:", answer);
+                    callback(answer);
+                });
+            }, 5000);
+        });
+    
+
+    });
+
+}
+
+module.exports = { process };
+/****************** Main function  ********/
+
+
+
