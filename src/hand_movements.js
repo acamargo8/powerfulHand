7d6a99905fe75a81d66ba26f557c220c1b9a6f8b
Thanasis <bbrthanasis@gmail.com>
2020-01-18 21:45:45 -0500

    Added scrolling and clicking detection

--- src/hand_movements.js
+++ src/hand_movements.js
@@ -1,7 +1,6 @@
 // Frame data class to hold hand information
 class FrameData {
-    constructor(joint_array)
-    {
+    constructor(joint_array){
         this.frame = joint_array.frame_time
         this.root = new Point2D(joint_array.x_root, joint_array.y_root)
         this.thumb = new Point2D(joint_array.x_thumb, joint_array.y_thumb)
@@ -14,8 +13,7 @@ class FrameData {
 
 // Helper class for calculations
 class Point2D {
-    constructor(xPos, yPos)
-    {
+    constructor(xPos, yPos){
         this.xPos = xPos
         this.yPos = yPos
     }
@@ -31,54 +29,224 @@ class Point2D {
     }
 }
 
-// To use for checking movement
+function checkForMovement(fdArray)
+{    
+    cm_counter = 0
+    cm_movement_counter = 0
+    dist_threshold = 0.008
+    mov_num_threshold = 10
+
+    for(frameData in fdArray)
+    {
+        if(cm_counter > 0)
+        {
+            if((Math.abs(fdArray[cm_counter].index.xPos - fdArray[cm_counter - 1].index.xPos) > dist_threshold)){
+                cm_movement_counter++
+            }
+        }
+        cm_counter++
+    }
+
+    if(cm_movement_counter > mov_num_threshold){
+        return "mouse_move"
+    }
+    return "no_mouse_move"
+}
+
+// Check if scroll gesture was performed
+function checkForScroll(fdArray){
+    sc_counter = 0
+    sc_movement_counter = 0
+    dist_threshold = 0.01
+    mov_num_threshold = 5
+
+    last_moved = false
+    max_consecutive_up_moves = 0
+    max_consecutive_down_moves = 0
+    consecutive_up_moves = 0
+    consecutive_down_moves = 0
+
+    for(frameData in fdArray){
+        if(sc_counter > 0){
+            if((Math.abs(fdArray[sc_counter].index.yPos - fdArray[sc_counter - 1].index.yPos) > dist_threshold)){
+                if(fdArray[sc_counter].index.yPos < fdArray[sc_counter - 1].index.yPos)
+                    consecutive_up_moves++
+                else
+                    consecutive_down_moves++
+
+                sc_movement_counter++
+                last_moved = true
+            }
+            else{
+                last_moved = false
+                if(consecutive_up_moves > max_consecutive_up_moves)
+                    max_consecutive_up_moves = consecutive_up_moves
+
+                if(consecutive_down_moves > max_consecutive_down_moves)
+                    max_consecutive_down_moves = consecutive_down_moves
+
+                consecutive_up_moves = 0
+                consecutive_down_moves = 0
+            }
+        }
+        sc_counter++
+        if(sc_counter >= fdArray.length)
+            break
+    }
+
+    console.log(sc_movement_counter)
+    console.log(max_consecutive_up_moves + " = max up moves")
+    console.log(max_consecutive_down_moves + " = max down moves")
+
+    // If hand moved both up and down, consider as no scroll
+    if(max_consecutive_down_moves > 3 && max_consecutive_up_moves > 3){
+        return "no_scroll"
+    }
+    
+    if(sc_movement_counter > mov_num_threshold && (max_consecutive_up_moves > 3 || max_consecutive_down_moves > 3)){
+        console.log(fdArray[0].index.yPos + "  " + fdArray[fdArray.length - 1].index.yPos)
+        if(fdArray[0].index.yPos < fdArray[fdArray.length - 1].index.yPos)
+            return "scroll_down"
+        else
+            return "scroll_up"
+    }
+    return "no_scroll"
+}
+
+//Determine if the gesture is left click
+function mouseLeftClick(fdArray){
+    for(frameData in fdArray){
+        if(counter >= 0){
+            if (fdArray[counter].thumb.xPos - fdArray[counter].index.xPos < 0.015 &&
+                fdArray[counter].thumb.yPos - fdArray[counter].index.yPos < 0.025)
+            {
+                total_true = total_true + 1;
+            } 
+            else{
+                total_false = total_false + 1;;
+            }
+        counter++
+        }
+    }
+    if (total_true/counter > 0.70){
+        left_click = true;
+        return left_click
+    } 
+    else{
+        return left_click
+    }
+}
+function mouseRightClick(fdArray){
+    for(frameData in fdArray){
+        if(counter >= 0){
+            if (fdArray[counter].thumb.xPos - fdArray[counter].middle.xPos < 0.020 &&
+                fdArray[counter].thumb.yPos - fdArray[counter].middle.yPos < 0.125)
+            {
+                total_true = total_true + 1;
+            } 
+            else{
+                total_false = total_false + 1;;
+            }
+        counter++
+        }
+    }
+    if (total_true/counter > 0.70){
+        right_click = true;
+        return right_click;
+    } 
+    else{
+        return right_click;
+    }
+}
+
+function checkForClick(fdArray)
+{
+    //console.log(!mouse_left_click(fdArray))
+    if (mouseLeftClick(fdArray)){
+        return "left_click"
+    } 
+    else {
+        total_true = 0;
+        total_false = 0;
+        counter = 0;
+        if (mouseRightClick(fdArray)){
+            return "right_click"
+        }
+        return "no_click"
+    }
+}
+
+// ===================================================
+// ================ Main Function ====================
+// ===================================================
+
 const THRESHOLD = 0.05
 
 //read json from a file
-var data = require('./Click2.json')
+var data = require('../mouse_simulation_data/mouse_scroll_down/mouse_scroll_down.json')
 const values = data["frames"];
 var num_frame = values.length
 var arr = [];
 var fdArray = [];
+
 for (i = 0; i < num_frame; i++){
     delete values[i].height;
     delete values[i].width;
-    arr.push({
-        frame_time : values[i].frame_time,
-        x_root : values[i].persons[0].hand_pose.right.joints[0],
-        y_root : values[i].persons[0].hand_pose.right.joints[1],
-        x_thumb : values[i].persons[0].hand_pose.right.joints[16],
-        y_thumb : values[i].persons[0].hand_pose.right.joints[17],
-        x_index : values[i].persons[0].hand_pose.right.joints[22],
-        y_index : values[i].persons[0].hand_pose.right.joints[23],
-        x_middle : values[i].persons[0].hand_pose.right.joints[28],
-        y_middle : values[i].persons[0].hand_pose.right.joints[29],
-        x_ring : values[i].persons[0].hand_pose.right.joints[34],
-        y_ring : values[i].persons[0].hand_pose.right.joints[35],
-        x_pinky : values[i].persons[0].hand_pose.right.joints[40],
-        y_pinky : values[i].persons[0].hand_pose.right.joints[41],
-    });
-
-    fdArray.push(new FrameData(arr[i]))
-}
-
-// console.log(fdArray[0].root.xPos)
-
-counter = 0
-static_gesture = false
-root_distances = []
-finger_distances = []
-
-movement_counter = 0 
-
-// Loop through frame data and check for movements/gestures
-for(frameData in fdArray)
-{
-    if(counter > 0)
-    {
-        console.log("root x pos: " + fdArray[counter].root.xPos + "  ||  root y pos: " + fdArray[counter].root.yPos)
+    try{
+        arr.push({
+            frame_time : values[i].frame_time,
+            x_root : values[i].persons[0].hand_pose.right.joints[0],
+            y_root : values[i].persons[0].hand_pose.right.joints[1],
+            x_thumb : values[i].persons[0].hand_pose.right.joints[16],
+            y_thumb : values[i].persons[0].hand_pose.right.joints[17],
+            x_index : values[i].persons[0].hand_pose.right.joints[22],
+            y_index : values[i].persons[0].hand_pose.right.joints[23],
+            x_middle : values[i].persons[0].hand_pose.right.joints[28],
+            y_middle : values[i].persons[0].hand_pose.right.joints[29],
+            x_ring : values[i].persons[0].hand_pose.right.joints[34],
+            y_ring : values[i].persons[0].hand_pose.right.joints[35],
+            x_pinky : values[i].persons[0].hand_pose.right.joints[40],
+            y_pinky : values[i].persons[0].hand_pose.right.joints[41],
+        });
+    
+        fdArray.push(new FrameData(arr[i]))
     }
-    counter++
+    catch(err)
+    {
+        console.log("Error parsing frame #" + i + ". Skipping...")
+    }
 }
+var gesture = ""
+gesture = checkForMovement(fdArray)
 
-// console.log(movement_counter)
+if(gesture == "mouse_move"){
+    for(frameData in fdArray){
+        // 
+        //Enter code to do the mouse movement
+        //
+    }
+    console.log(gesture)
+}
+else{
+    gesture = checkForScroll(fdArray)
+
+    if(gesture == "scroll_down" || gesture == "scroll_up"){
+        console.log(gesture)
+    }
+    else{
+        //Variables for checkForClick function
+        counter = 0;
+        var total_true = 0;
+        var total_false = 0;
+        var left_click = false;
+        var right_click = false;
+
+        gesture = checkForClick(fdArray)
+
+        if(gesture == "left_click" || gesture == "right_click")
+            console.log(gesture)
+        
+        else
+            console.log("no_gesture")
+    }
+}
